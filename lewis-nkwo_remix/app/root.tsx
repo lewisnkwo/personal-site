322971487c81bc73ce5a243ffc829251ae56af02
Lewis Nkwo <lewisnkwo@Lewiss-MBP.cust.communityfibre.co.uk>
2023-03-30 18:38:54 +0100

    Add remaining css file paths

--- lewis-nkwo_remix/app/root.tsx
+++ lewis-nkwo_remix/app/root.tsx
@@ -8,6 +8,14 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 import layoutStyles from "./components/pages/layout/index.css";
+import headerStyles from "./components/shared/header/index.css";
+import mainStyles from "./components/shared/main/index.css";
+import postItemStyles from "./components/shared/main/post-item/index.css";
+import footerStyles from "./components/shared/footer/index.css";
+import imageStyles from "./components/shared/image/index.css";
+import sidebarStyles from "./components/shared/sidebar/index.css";
+import sidebarItemStyles from "./components/shared/sidebar/sidebar-item/index.css";
+import sidebarDetailStyles from "./components/shared/sidebar-detail/index.css";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
@@ -16,7 +24,17 @@ export const meta: MetaFunction = () => ({
 });
 
 export const links: LinksFunction = () => {
-  return [{ rel: "stylesheet", href: layoutStyles }];
+  return [
+    { rel: "stylesheet", href: layoutStyles },
+    { rel: "stylesheet", href: headerStyles },
+    { rel: "stylesheet", href: mainStyles },
+    { rel: "stylesheet", href: postItemStyles },
+    { rel: "stylesheet", href: footerStyles },
+    { rel: "stylesheet", href: imageStyles },
+    { rel: "stylesheet", href: sidebarStyles },
+    { rel: "stylesheet", href: sidebarItemStyles },
+    { rel: "stylesheet", href: sidebarDetailStyles },
+  ];
 };
 
 export default function App() {
