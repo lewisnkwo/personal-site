44c92ce36c5536576e169bc6ef9d2c0c7d373c62
Lewis Nkwo <lewisnkwo@Lewiss-MBP.cust.communityfibre.co.uk>
2023-06-03 15:23:26 +0100

    Add about me page

---
+++ app/routes/about.tsx
@@ -0,0 +1,23 @@
+import Layout from "~/components/pages/layout";
+import type { LinksFunction, MetaFunction } from "@remix-run/node";
+import aboutStyles from "../components/pages/content/about/index.css";
+import About from "~/components/pages/content/about";
+
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: aboutStyles }];
+};
+
+export const meta: MetaFunction = () => ({
+  title: "Lewis Nkwo | About",
+  description: "A little bit about me!",
+});
+
+export default function AboutRoute() {
+  return (
+    <Layout>
+      <main>
+        <About />
+      </main>
+    </Layout>
+  );
+}
